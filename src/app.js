f0a9b6829364c8453ecdbc570c763ec94cc52eec
Swetank <swetank199@gmail.com>
2022-09-01 05:21:16 +0530

    Added Basic config

---
+++ src/app.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+//parse json requrest body
+app.use(express.json());
+//parse url encoded request body
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
+app.options("*", cors());
+
+/*app.use(passport.initialize());
+passport.use(
+	"jwt",
+	new JwtStrategy(jwtOptions, (jwt_payload, done) => {
+		User.findById(jwt_payload.id)
+			.then((user) => {
+				if (user) {
+					return done(null, user);
+				}
+				return done(null, false);
+			})
+			.catch((err) => {
+				console.log(err);
+				return done(err, false);
+			});
+	})
+);
+passport.use(
+	"local",
+	new LocalStrategy(localOptions, (username, password, done) => {
+		User.findOne({ username }).then((user) => {
+			if (!user) {
+				return done(null, false);
+			}
+			user
+				.comparePassword(password, (err, isMatch) => {
+					if (err) {
+						return done(err);
+					}
+					if (!isMatch) {
+						return done(null, false);
+					}
+					return done(null, user);
+				})
+				.catch((err) => {
+					console.log(err);
+					return done(err, false);
+				});
+		});
+	}).catch((err) => {
+		console.log(err);
+		return done(err, false);
+	})
+);
+*/
+module.exports = app;
---
+++ src/index.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+const app = require("./app");
+const config = require("./config/config");
+
+let server;
+mongoose.connect(config.mongoose.url, config.mongoose.options).then(() => {
+	console.log("Config", config);
+	server = app.listen(config.port, () => {
+		console.log(`Server is running on port ${config.port}`);
+	});
+});
+
+const exitHandler = () => {
+	if (server) {
+		server.close(() => {
+			console.log("Server closed");
+			process.exit(1);
+		});
+	} else {
+		process.exit(1);
+	}
+};
+
+const unexpectedErrorHandler = (err) => {
+	console.log(err);
+	exitHandler();
+};
+
+process.on("uncaughtException", unexpectedErrorHandler);
+process.on("unhandledRejection", unexpectedErrorHandler);
+
+process.on("SIGTERM", () => {
+	console.log("SIGTERM signal received.");
+	if (server) {
+		server.close(() => {
+			process.exit(1);
+		});
+	}
+});
+
+process.on("SIGINT", function () {
+	console.log("\nGracefully shutting down from SIGINT (Ctrl-C)");
+	// some other closing procedures go here
+	process.exit(0);
+});
